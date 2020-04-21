49f408bf448b9fc63faf076dc6462b5a93b5e04f
Ron B <me@ronthecookie.me>
2020-04-21 13:38:17 +0300

    work on plugin.ts

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,41 +2,60 @@ import BaseManager from "./baseManager";
 import Minehut from ".";
 
 interface Plugin {
-    id: string;
-    name: string;
-    credits: number;
-    platform: "java";
-    desc: string;
-    descExtended: string;
-    version: string;
-    disabled: boolean;
-    fileName: string;
-    configFileName: string;
-    created: Date;
-    lastUpdated: Date;
+	id: string;
+	name: string;
+	credits: number;
+	platform: "java";
+	description: string;
+	extendedDescription: string;
+	version: string;
+	disabled: boolean;
+	fileName: string;
+	configFileName: string;
+	created: Date;
+	lastUpdated: Date;
 }
 
 interface RawPlugin {
-    id: string;
-    name: string;
-    credits: number;
-    platform: "java";
-    desc: string;
-    desc_extended: string;
-    version: string;
-    disabled: boolean;
-    fileName: string;
-    config_file_name: string;
-    created: number;
-    last_updated: number;
+	_id: string;
+	name: string;
+	credits: number;
+	platform: "java";
+	desc: string;
+	desc_extended: string;
+	version: string;
+	disabled: boolean;
+	fileName: string;
+	config_file_name: string;
+	created: number;
+	last_updated: number;
 }
 
-export class PluginManager extends BaseManager<RawPlugin[], Plugin[]> {
-    constructor(client: Minehut) {
-        super(client, "/plugins_public")
-    }
+interface RawPluginResponse {
+	all: RawPlugin[];
+	java: RawPlugin[];
+	bedrock: RawPlugin[];
+}
 
-    async transform(data: RawPlugin[]): Promise<Plugin[]> {
-        
-    }
+export class PluginManager extends BaseManager<RawPluginResponse, Plugin[]> {
+	constructor(client: Minehut) {
+		super(client, "/plugins_public");
+	}
+
+	async transform(data: RawPluginResponse): Promise<Plugin[]> {
+		return data.all.map((x) => ({
+			configFileName: x.config_file_name,
+			created: new Date(x.created),
+			credits: x.credits,
+			description: x.desc,
+			extendedDescription: x.desc_extended,
+			disabled: x.disabled,
+			fileName: x.fileName,
+			id: x._id,
+			lastUpdated: new Date(x.last_updated),
+			name: x.name,
+			platform: x.platform,
+			version: x.version,
+		}));
+	}
 }
